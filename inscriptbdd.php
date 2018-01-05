66bd3b4420107b2d7aadebfe5c62bbbcfa1c2265
Ftoumi <fawzi.t@hotmail.fr>
2018-01-05 15:25:05 +0100

    mise a jour vendredi 15h24 mise en marche de la verification du mot de pass inscription+ ecriture sur la bdd dans différente table

--- inscriptbdd.php
+++ inscriptbdd.php
@@ -1,36 +1,60 @@
 <?php
-//on récupère les valeurs du formulaire
+/*on récupère les valeurs du formulaire*/
 
 $nom = $_POST['nom'];
 
 $prenom = $_POST['prenom'];
+$ville = $_POST['ville'];
+$pays = $_POST['pays'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+/*$club = $_POST['club'];
+$numfff = $_POST['number'];
+$categorie = $_POST['categorie'];
+$niveau = $_POST['niveau'];*/
+$identifiant = $_POST['identifiant'];
+$mdp = $_POST['password'];
+$mdpv = $_POST['vpassword'];
 
+/*vérification du mot de passe*/
+ if($_POST['password'] ==$_POST['vpassword'])
+  {
+/*Connexion à la base de donnée*/
  try
- {//Connexion à la base de donnée
+ {
 
 	$bdd = new PDO('mysql:host=localhost;dbname=statisfoot;charset=utf8', 'statisfoot', 'yjnRTeqXKgStt29S');
-     /*
-    $sql = "INSERT INTO `utilisateur` (`nom`, `prenom`) VALUES ('$nom', '$prenom')";
-       */ 
 
-
-} catch (PDOException $e) {
+} 
+catch (Exception $e)
+{
     http_response_code(500);
     die();
-}
- 
-/*
-mysql_connect("localhost", "statisfoot", "yjnRTeqXKgStt29S");
-mysql_select_db("statisfoot");
- */
- 
-if(isset($_POST["nom"]) and isset($_POST['prenom']))
-    {  
-    /*
-                                    $nom = addslashes(mysql_real_escape_string($_POST['nom']));
-                    $prenom = addslashes(mysql_real_escape_string($_POST['prenom']));
-                    */
-                             
-        $result =$bdd->exec("INSERT INTO utilisateur (nom, prenom) VALUES('" . $nom . "', '" . $prenom . "')");
-}
+}          /*ecire table entraineur*/                   
+        $req =$bdd->prepare("INSERT INTO entraineurs(nom, prenom, ville, pays, tel, email) VALUES(:nom, :prenom, :ville, :pays, :tel, :email)");
+        $req->execute(array(
+        'nom' => $nom,
+        'prenom' => $prenom,
+        'ville' => $ville,
+        'pays' => $pays,
+        'tel' => $tel,
+        'email' => $email,  
+            
+        ));
+     /*ecrire table membres*/
+      $req_user=$bdd->prepare("INSERT INTO membres(identifiant, motdepasse) VALUES(:identifiant, :motdepasse)");
+        $req_user->execute(array(
+        'identifiant' => $identifiant,
+        'motdepasse' => $mdp, 
+            
+             ));
+     
+     
+     
+  echo 'inscription validée !';
+ }
+   else
+    {
+     echo 'mots de passes pas identiques retrourner sur la page précedente';
+                      } 
 ?>
